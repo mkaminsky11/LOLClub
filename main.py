7239ce3f3632634af3c771a3bda9025007801865
Michael Kaminsky <mkaminsky11@gmail.com>
2016-09-10 23:22:48 -0400

    getting started

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def index(user=None):
+    return render_template('index.html')
+
+@app.route('/<path:path>')
+def static_proxy(path):
+  return app.send_static_file(path)
+
+if __name__ == '__main__':
+    app.run()
